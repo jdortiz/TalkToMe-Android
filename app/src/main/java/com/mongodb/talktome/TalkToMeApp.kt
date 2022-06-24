41d031dad3ef78078c1a68cb94f0b436616666c6
Jorge D. Ortiz Fuentes <jorge.ortiz-fuentes@mongodb.com>
2022-06-24 17:57:49 +0200

    Open Synced Realm

---
+++ app/src/main/java/com/mongodb/talktome/TalkToMeApp.kt
@@ -0,0 +1,36 @@
+package com.mongodb.talktome
+
+import android.app.Application
+import android.util.Log
+import com.mongodb.talktome.model.Talk
+import io.realm.kotlin.Realm
+import io.realm.kotlin.internal.platform.runBlocking
+import io.realm.kotlin.mongodb.App
+import io.realm.kotlin.mongodb.Credentials
+import io.realm.kotlin.mongodb.sync.SyncConfiguration
+
+class TalkToMeApp : Application() {
+    lateinit var realm: Realm
+
+    companion object {
+        const val APP_ID = "application-0-guxux"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        val app = App.create(APP_ID)
+        runBlocking {
+            val user = app.login(Credentials.anonymous())
+            val config = SyncConfiguration.Builder(
+                user = user,
+                partitionValue = app.currentUser?.identity,
+                schema = setOf(Talk::class)
+            )
+                .maxNumberOfActiveVersions(10)
+                .waitForInitialRemoteData()
+                .build()
+            realm = Realm.open(config)
+            Log.v("Realm", "Successfully opened realm: ${realm.configuration}")
+        }
+    }
+}
